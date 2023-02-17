065f89704daf76e2f2cac6dd04de5f8b15de8aab
RahmanSopian <104488328+rahmansopianqa@users.noreply.github.com>
2023-02-17 15:20:07 +0700

    update

--- cypress/support/e2e/addCustomer.js
+++ cypress/support/e2e/addCustomer.js
@@ -3,7 +3,7 @@ import faker from 'faker';
 const nama = faker.name.findName()
 const alamat = faker.address.streetAddress()
 const phoneNumber = faker.phone.phoneNumber('08##########');
-const keterangan = faker.lorem.paragraph()
+const keterangan = faker.lorem.lines(1)
 
 describe("Add Customer Test Scenario", ()=>{
     it('User success add customer to kasir aja', ()=>{
@@ -11,7 +11,7 @@ describe("Add Customer Test Scenario", ()=>{
         cy.get('#email').type(Cypress.env('email'))
         cy.get('#password').type(Cypress.env('password'))
         cy.get('#root > div > div > div > div.css-1w7v3tn > div > button').click()
-        cy.get(':nth-child(9) > .css-ewi1jp > .css-1xhj18k > .css-1mqa38q').click()
+        cy.contains('pelanggan').click()
         cy.contains('tambah').click()
         cy.get('#nama').type(nama)
         cy.get('[id$="no.hp"]').type(phoneNumber)
@@ -20,12 +20,12 @@ describe("Add Customer Test Scenario", ()=>{
         cy.contains('simpan').click()
         cy.contains('item ditambahkan', { timeout: 10000 })
     }),
-    it('User unsuccess add customer to kasir aja when data empty', ()=>{
+    it('User unsuccess add customer to kasir aja when data is empty', ()=>{
         cy.visit(Cypress.env('baseUrl'))
         cy.get('#email').type(Cypress.env('email'))
         cy.get('#password').type(Cypress.env('password'))
         cy.get('#root > div > div > div > div.css-1w7v3tn > div > button').click()
-        cy.get(':nth-child(9) > .css-ewi1jp > .css-1xhj18k > .css-1mqa38q').click()
+        cy.contains('pelanggan').click()
         cy.contains('tambah').click()
         cy.contains('simpan').click()
         cy.contains('"name" is not allowed to be empty')
